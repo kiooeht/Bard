12bef166f050164a393947d08ad0b522ff2a5d65
Kiooeht <kiooeht@gmail.com>
2018-11-26 23:03:43 -0800

    Card: Buckler Bash

---
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/cards/BucklerBash.java
@@ -0,0 +1,63 @@
+package com.evacipated.cardcrawl.mod.bard.cards;
+
+import com.evacipated.cardcrawl.mod.bard.BardMod;
+import com.evacipated.cardcrawl.mod.bard.characters.Bard;
+import com.evacipated.cardcrawl.mod.bard.notes.AbstractNote;
+import com.evacipated.cardcrawl.mod.bard.notes.AttackNote;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.utility.LoseBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.Collections;
+import java.util.List;
+
+public class BucklerBash extends AbstractBardCard
+{
+    public static final String ID = BardMod.makeID("BucklerBash");
+    public static final String IMG = null;
+    private static final int COST = 1;
+    private static final int BLOCK_LOSS = 5;
+    private static final int UPGRADE_BLOCK_LOSS = 3;
+    private static final int DAMAGE = 12;
+    private static final int UPGRADE_DAMAGE = 6;
+
+    public BucklerBash()
+    {
+        super(ID, IMG, COST, CardType.ATTACK, Bard.Enums.COLOR, CardRarity.COMMON, CardTarget.SELF_AND_ENEMY);
+
+        baseDamage = DAMAGE;
+        magicNumber = baseMagicNumber = BLOCK_LOSS;
+    }
+
+    @Override
+    public List<AbstractNote> getNotes()
+    {
+        return Collections.singletonList(new AttackNote());
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        addToBottom(new LoseBlockAction(p, p, magicNumber));
+        addToBottom(new DamageAction(m, new DamageInfo(p, baseDamage)));
+    }
+
+    @Override
+    public void upgrade()
+    {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_DAMAGE);
+            upgradeMagicNumber(UPGRADE_BLOCK_LOSS);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy()
+    {
+        return new BucklerBash();
+    }
+}
