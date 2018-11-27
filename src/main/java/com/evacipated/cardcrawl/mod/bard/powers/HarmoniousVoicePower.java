743037bdc9886314059688be45a86bcb29c68c2f
Kiooeht <kiooeht@gmail.com>
2018-11-26 18:24:07 -0800

    Card: Harmonious Voice

---
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/powers/HarmoniousVoicePower.java
@@ -0,0 +1,49 @@
+package com.evacipated.cardcrawl.mod.bard.powers;
+
+import com.evacipated.cardcrawl.mod.bard.BardMod;
+import com.evacipated.cardcrawl.mod.bard.notes.AbstractNote;
+import com.evacipated.cardcrawl.mod.bard.notes.BlockNote;
+import com.evacipated.cardcrawl.mod.bard.notes.BuffNote;
+import com.evacipated.cardcrawl.mod.bard.powers.interfaces.OnNoteQueuedPower;
+import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class HarmoniousVoicePower extends AbstractPower implements OnNoteQueuedPower
+{
+    public static final String POWER_ID = BardMod.makeID("HarmoniousVoice");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public HarmoniousVoicePower(AbstractCreature owner, int tempHp)
+    {
+        name = NAME;
+        ID = POWER_ID;
+        this.owner = owner;
+        type = PowerType.BUFF;
+        amount = tempHp;
+        updateDescription();
+        // TODO
+        loadRegion("penNib");
+    }
+
+    @Override
+    public void updateDescription()
+    {
+        description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+    }
+
+    @Override
+    public AbstractNote onNoteQueued(AbstractNote note)
+    {
+        if (note instanceof BlockNote || note instanceof BuffNote) {
+            AbstractDungeon.actionManager.addToBottom(new AddTemporaryHPAction(owner, owner, amount));
+        }
+
+        return note;
+    }
+}
