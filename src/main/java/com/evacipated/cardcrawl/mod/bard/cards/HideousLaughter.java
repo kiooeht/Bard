18fce1297bbb70b8d5482911cf25440b08837ce0
Kiooeht <kiooeht@gmail.com>
2018-11-26 19:15:47 -0800

    Card: Hideous Laughter

---
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/cards/HideousLaughter.java
@@ -0,0 +1,59 @@
+package com.evacipated.cardcrawl.mod.bard.cards;
+
+import com.evacipated.cardcrawl.mod.bard.BardMod;
+import com.evacipated.cardcrawl.mod.bard.characters.Bard;
+import com.evacipated.cardcrawl.mod.bard.notes.AbstractNote;
+import com.evacipated.cardcrawl.mod.bard.notes.DebuffNote;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+
+import java.util.Collections;
+import java.util.List;
+
+public class HideousLaughter extends AbstractBardCard
+{
+    public static final String ID = BardMod.makeID("HideousLaughter");
+    public static final String IMG = null;
+    private static final int COST = 0;
+    private static final int DEBUFF_AMT = 1;
+    private static final int UPGRADE_DEBUFF_AMT = 1;
+
+    public HideousLaughter()
+    {
+        super(ID, IMG, COST, CardType.SKILL, Bard.Enums.COLOR, CardRarity.COMMON, CardTarget.ENEMY);
+
+        magicNumber = baseMagicNumber = DEBUFF_AMT;
+    }
+
+    @Override
+    public List<AbstractNote> getNotes()
+    {
+        return Collections.singletonList(new DebuffNote());
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        addToBottom(new ApplyPowerAction(m, p, new VulnerablePower(m, magicNumber, false), magicNumber));
+        addToBottom(new ApplyPowerAction(m, p, new WeakPower(m, magicNumber, false), magicNumber));
+    }
+
+    @Override
+    public void upgrade()
+    {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_DEBUFF_AMT);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy()
+    {
+        return new HideousLaughter();
+    }
+}
