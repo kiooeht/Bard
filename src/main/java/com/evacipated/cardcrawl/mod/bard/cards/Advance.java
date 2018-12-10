f4412ae45f9237ef0e510b76736c405d03095cd9
Kiooeht <kiooeht@gmail.com>
2018-12-09 16:58:15 -0800

    Card: Advance

---
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/cards/Advance.java
@@ -0,0 +1,68 @@
+package com.evacipated.cardcrawl.mod.bard.cards;
+
+import com.evacipated.cardcrawl.mod.bard.BardMod;
+import com.evacipated.cardcrawl.mod.bard.characters.Bard;
+import com.evacipated.cardcrawl.mod.bard.notes.AbstractNote;
+import com.evacipated.cardcrawl.mod.bard.notes.BuffNote;
+import com.evacipated.cardcrawl.mod.bard.notes.DebuffNote;
+import com.evacipated.cardcrawl.mod.bard.powers.DeEnergizedPower;
+import com.evacipated.cardcrawl.mod.stslib.fields.cards.AbstractCard.AlwaysRetainField;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Advance extends AbstractBardCard
+{
+    public static final String ID = BardMod.makeID("Advance");
+    public static final String IMG = null;
+    private static final int COST = 0;
+    private static final int ENERGY = 1;
+    private static final int DRAW = 2;
+
+    public Advance()
+    {
+        super(ID, IMG, COST, CardType.SKILL, Bard.Enums.COLOR, CardRarity.UNCOMMON, CardTarget.SELF);
+
+        magicNumber = baseMagicNumber = DRAW;
+    }
+
+    @Override
+    public List<AbstractNote> getNotes()
+    {
+        return Arrays.asList(
+                new BuffNote(),
+                new DebuffNote()
+        );
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        addToBottom(new GainEnergyAction(ENERGY));
+        addToBottom(new DrawCardAction(p, magicNumber));
+        addToBottom(new ApplyPowerAction(p, p, new DeEnergizedPower(p, ENERGY)));
+    }
+
+    @Override
+    public void upgrade()
+    {
+        if (!upgraded) {
+            upgradeName();
+            rawDescription = UPGRADE_DESCRIPTION + DESCRIPTION;
+            initializeDescription();
+            AlwaysRetainField.alwaysRetain.set(this, true);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy()
+    {
+        return new Advance();
+    }
+}
