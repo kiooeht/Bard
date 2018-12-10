e16d316daf749bbb64ace72a6044d167a6365499
Kiooeht <kiooeht@gmail.com>
2018-12-09 22:12:35 -0800

    Card: Guard Point

---
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/cards/GuardPoint.java
@@ -0,0 +1,64 @@
+package com.evacipated.cardcrawl.mod.bard.cards;
+
+import com.evacipated.cardcrawl.mod.bard.BardMod;
+import com.evacipated.cardcrawl.mod.bard.characters.Bard;
+import com.evacipated.cardcrawl.mod.bard.notes.AbstractNote;
+import com.evacipated.cardcrawl.mod.bard.notes.BlockNote;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.Collections;
+import java.util.List;
+
+public class GuardPoint extends AbstractBardCard
+{
+    public static final String ID = BardMod.makeID("GuardPoint");
+    public static final String IMG = null;
+    private static final int COST = 1;
+    private static final int BLOCK = 7;
+    private static final int UPGRADE_BLOCK = 2;
+
+    public GuardPoint()
+    {
+        super(ID, IMG, COST, CardType.SKILL, Bard.Enums.COLOR, CardRarity.UNCOMMON, CardTarget.SELF);
+
+        baseBlock = BLOCK;
+    }
+
+    @Override
+    public List<AbstractNote> getNotes()
+    {
+        return Collections.singletonList(new BlockNote());
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        addToBottom(new GainBlockAction(p, p, block));
+        if (p instanceof Bard) {
+            int count = ((Bard) p).noteQueueCount(BlockNote.class);
+            if (count == 0) {
+                addToBottom(new GainBlockAction(p, p, block));
+            }
+        } else {
+            addToBottom(new GainBlockAction(p, p, block));
+        }
+    }
+
+    @Override
+    public void upgrade()
+    {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_BLOCK);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy()
+    {
+        return new GuardPoint();
+    }
+}
