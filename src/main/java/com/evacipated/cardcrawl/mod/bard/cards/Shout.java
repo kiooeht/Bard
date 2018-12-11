b48e0ba7a0dfa228d70a805a3da1a8cb3e805d03
Kiooeht <kiooeht@gmail.com>
2018-12-10 18:06:04 -0800

    Card: Shout

---
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/cards/Shout.java
@@ -0,0 +1,70 @@
+package com.evacipated.cardcrawl.mod.bard.cards;
+
+import com.evacipated.cardcrawl.mod.bard.BardMod;
+import com.evacipated.cardcrawl.mod.bard.characters.Bard;
+import com.evacipated.cardcrawl.mod.bard.notes.AbstractNote;
+import com.evacipated.cardcrawl.mod.bard.notes.AttackNote;
+import com.evacipated.cardcrawl.mod.bard.notes.DebuffNote;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Shout extends AbstractBardCard
+{
+    public static final String ID = BardMod.makeID("Shout");
+    public static final String IMG = null;
+    private static final int COST = 2;
+    private static final int DAMAGE = 12;
+    private static final int UPGRADE_DAMAGE = 4;
+    private static final int STR_LOSS = 2;
+
+    public Shout()
+    {
+        super(ID, IMG, COST, CardType.ATTACK, Bard.Enums.COLOR, CardRarity.UNCOMMON, CardTarget.ENEMY);
+
+        baseDamage = DAMAGE;
+        magicNumber = baseMagicNumber = STR_LOSS;
+    }
+
+    @Override
+    public List<AbstractNote> getNotes()
+    {
+        return Arrays.asList(
+                new AttackNote(),
+                new DebuffNote()
+        );
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        addToBottom(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn)));
+        if (magicNumber > 0) {
+            addToBottom(new ApplyPowerAction(m, p, new StrengthPower(m, -magicNumber), -magicNumber));
+            --baseMagicNumber;
+            magicNumber = baseMagicNumber;
+        }
+    }
+
+    @Override
+    public void upgrade()
+    {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_DAMAGE);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy()
+    {
+        return new Shout();
+    }
+}
