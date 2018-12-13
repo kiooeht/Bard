22c2936408b2815d4f73037afd2b438283dd9fd3
Kiooeht <kiooeht@gmail.com>
2018-12-12 22:37:40 -0800

    Card: Blink

---
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/cards/Blink.java
@@ -0,0 +1,59 @@
+package com.evacipated.cardcrawl.mod.bard.cards;
+
+import com.evacipated.cardcrawl.mod.bard.BardMod;
+import com.evacipated.cardcrawl.mod.bard.characters.Bard;
+import com.evacipated.cardcrawl.mod.bard.notes.AbstractNote;
+import com.evacipated.cardcrawl.mod.bard.notes.BuffNote;
+import com.evacipated.cardcrawl.mod.bard.powers.BlinkPower;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Blink extends AbstractBardCard
+{
+    public static final String ID = BardMod.makeID("Blink");
+    public static final String IMG = null;
+    private static final int COST = 1;
+    private static final int DAMAGE_REDUCE = 50;
+    private static final int TURNS = 1;
+    private static final int UPGRADE_TURNS = 1;
+
+    public Blink()
+    {
+        super(ID, IMG, COST, CardType.SKILL, Bard.Enums.COLOR, CardRarity.UNCOMMON, CardTarget.SELF);
+
+        magicNumber2 = baseMagicNumber2 = DAMAGE_REDUCE;
+        magicNumber = baseMagicNumber = TURNS;
+    }
+
+    @Override
+    public List<AbstractNote> getNotes()
+    {
+        return Collections.singletonList(new BuffNote());
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        addToBottom(new ApplyPowerAction(p, p, new BlinkPower(p, magicNumber), magicNumber));
+    }
+
+    @Override
+    public void upgrade()
+    {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_TURNS);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy()
+    {
+        return new Blink();
+    }
+}
---
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/powers/BlinkPower.java
@@ -0,0 +1,60 @@
+package com.evacipated.cardcrawl.mod.bard.powers;
+
+import com.evacipated.cardcrawl.mod.bard.BardMod;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class BlinkPower extends AbstractPower
+{
+    public static final String POWER_ID = BardMod.makeID("Blink");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public BlinkPower(AbstractCreature owner, int amount)
+    {
+        name = NAME;
+        ID = POWER_ID;
+        this.owner = owner;
+        type = PowerType.BUFF;
+        this.amount = amount;
+        isTurnBased = true;
+        updateDescription();
+        region48 = BardMod.powerAtlas.findRegion("48/blink");
+        region128 = BardMod.powerAtlas.findRegion("128/blink");
+        priority = 98;
+    }
+
+    @Override
+    public void updateDescription()
+    {
+        if (amount == 1) {
+            description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+        } else {
+            description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[2];
+        }
+    }
+
+    @Override
+    public float atDamageReceive(float damage, DamageInfo.DamageType type)
+    {
+        return damage / 2.0f;
+    }
+
+    @Override
+    public void atEndOfRound()
+    {
+        flash();
+        if (amount == 0) {
+            AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(owner, owner, this));
+        } else {
+            AbstractDungeon.actionManager.addToBottom(new ReducePowerAction(owner, owner, this, 1));
+        }
+    }
+}
