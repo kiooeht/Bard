1177a9a2a5d841fbbf66921ddc4d95560ff0339a
Kiooeht <kiooeht@gmail.com>
2018-12-12 19:32:15 -0800

    Card: Splendid Form

---
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/cards/SplendidForm.java
@@ -0,0 +1,60 @@
+package com.evacipated.cardcrawl.mod.bard.cards;
+
+import basemod.helpers.BaseModCardTags;
+import com.evacipated.cardcrawl.mod.bard.BardMod;
+import com.evacipated.cardcrawl.mod.bard.characters.Bard;
+import com.evacipated.cardcrawl.mod.bard.notes.AbstractNote;
+import com.evacipated.cardcrawl.mod.bard.notes.BuffNote;
+import com.evacipated.cardcrawl.mod.bard.powers.SplendidPower;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SplendidForm extends AbstractBardCard
+{
+    public static final String ID = BardMod.makeID("SplendidForm");
+    public static final String IMG = null;
+    private static final int COST = 3;
+    private static final int INSPIRATION = 100;
+    private static final int UPGRADE_INSPIRATION = 100;
+
+    public SplendidForm()
+    {
+        super(ID, IMG, COST, CardType.POWER, Bard.Enums.COLOR, CardRarity.RARE, CardTarget.SELF);
+
+        tags.add(BaseModCardTags.FORM);
+
+        magicNumber2 = baseMagicNumber2 = INSPIRATION;
+    }
+
+    @Override
+    public List<AbstractNote> getNotes()
+    {
+        return Collections.singletonList(new BuffNote());
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        addToBottom(new ApplyPowerAction(p, p, new SplendidPower(p, magicNumber2, 1), 1));
+    }
+
+    @Override
+    public void upgrade()
+    {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber2(UPGRADE_INSPIRATION);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy()
+    {
+        return new SplendidForm();
+    }
+}
---
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/powers/SplendidPower.java
@@ -0,0 +1,61 @@
+package com.evacipated.cardcrawl.mod.bard.powers;
+
+import com.evacipated.cardcrawl.mod.bard.BardMod;
+import com.evacipated.cardcrawl.mod.bard.powers.interfaces.NonStackablePower;
+import com.evacipated.cardcrawl.mod.bard.powers.interfaces.TwoAmountPower;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class SplendidPower extends TwoAmountPower implements NonStackablePower
+{
+    public static final String POWER_ID = BardMod.makeID("Splendid");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public SplendidPower(AbstractCreature owner, int inspiration, int amount)
+    {
+        name = NAME;
+        ID = POWER_ID;
+        this.owner = owner;
+        type = PowerType.BUFF;
+        this.amount = amount;
+        this.amount2 = inspiration;
+        updateDescription();
+        region48 = BardMod.powerAtlas.findRegion("48/splendid");
+        region128 = BardMod.powerAtlas.findRegion("128/splendid");
+    }
+
+    @Override
+    public void updateDescription()
+    {
+        if (amount == 1) {
+            description = DESCRIPTIONS[0] + amount2 + DESCRIPTIONS[3];
+        } else {
+            description = DESCRIPTIONS[1] + amount + DESCRIPTIONS[2] + amount2 + DESCRIPTIONS[3];
+        }
+    }
+
+    @Override
+    public void atStartOfTurn()
+    {
+        AbstractPlayer p = AbstractDungeon.player;
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new InspirationPower(p, amount, amount2), amount));
+    }
+
+    @Override
+    public boolean isStackable(AbstractPower power)
+    {
+        if (power instanceof SplendidPower) {
+            if (amount2 == ((SplendidPower) power).amount2) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
