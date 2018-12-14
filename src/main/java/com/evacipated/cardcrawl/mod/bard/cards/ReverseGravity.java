66c6f78f992e05a07639e43a12e8e55b4f23fbdf
Kiooeht <kiooeht@gmail.com>
2018-12-13 21:32:16 -0800

    Card: Reverse Gravity

---
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/cards/ReverseGravity.java
@@ -0,0 +1,74 @@
+package com.evacipated.cardcrawl.mod.bard.cards;
+
+import com.evacipated.cardcrawl.mod.bard.BardMod;
+import com.evacipated.cardcrawl.mod.bard.characters.Bard;
+import com.evacipated.cardcrawl.mod.bard.notes.AbstractNote;
+import com.evacipated.cardcrawl.mod.bard.notes.AttackNote;
+import com.evacipated.cardcrawl.mod.bard.notes.DebuffNote;
+import com.evacipated.cardcrawl.mod.bard.powers.ReverseGravityPower;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReverseGravity extends AbstractBardCard
+{
+    public static final String ID = BardMod.makeID("ReverseGravity");
+    public static final String IMG = null;
+    private static final int COST = 3;
+    private static final int UPGRADE_COST = 2;
+    private static final int DAMAGE = 15;
+    private static final int WEAK = 1;
+
+    public ReverseGravity()
+    {
+        super(ID, IMG, COST, CardType.ATTACK, Bard.Enums.COLOR, CardRarity.RARE, CardTarget.ALL_ENEMY);
+
+        baseDamage = DAMAGE;
+        isMultiDamage = true;
+        magicNumber = baseMagicNumber = WEAK;
+        magicNumber2 = baseMagicNumber2 = DAMAGE;
+    }
+
+    @Override
+    public List<AbstractNote> getNotes()
+    {
+        return Arrays.asList(
+                new AttackNote(),
+                new DebuffNote(),
+                new AttackNote()
+        );
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        addToBottom(new DamageAllEnemiesAction(p, multiDamage, damageTypeForTurn, AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+        for (AbstractMonster mo : AbstractDungeon.getMonsters().monsters) {
+            addToBottom(new ApplyPowerAction(mo, p, new WeakPower(mo, magicNumber, false), magicNumber, true, AbstractGameAction.AttackEffect.NONE));
+        }
+        addToBottom(new ApplyPowerAction(p, p, new ReverseGravityPower(p, magicNumber2, magicNumber), magicNumber2));
+    }
+
+    @Override
+    public void upgrade()
+    {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADE_COST);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy()
+    {
+        return new ReverseGravity();
+    }
+}
---
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/powers/ReverseGravityPower.java
@@ -0,0 +1,57 @@
+package com.evacipated.cardcrawl.mod.bard.powers;
+
+import com.evacipated.cardcrawl.mod.bard.BardMod;
+import com.evacipated.cardcrawl.mod.bard.powers.interfaces.NonStackablePower;
+import com.evacipated.cardcrawl.mod.bard.powers.interfaces.TwoAmountPower;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
+
+public class ReverseGravityPower extends TwoAmountPower implements NonStackablePower
+{
+    public static final String POWER_ID = BardMod.makeID("ReverseGravity");
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public ReverseGravityPower(AbstractCreature owner, int damage, int weak)
+    {
+        name = NAME;
+        ID = POWER_ID;
+        this.owner = owner;
+        type = PowerType.BUFF;
+        amount2 = damage;
+        amount = weak;
+        isTurnBased = true;
+        updateDescription();
+        // TODO
+        loadRegion("rupture");
+    }
+
+    @Override
+    public void updateDescription()
+    {
+        description = String.format(DESCRIPTIONS[0], amount2, amount);
+    }
+
+    @Override
+    public void atStartOfTurn()
+    {
+        if (!AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
+            AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(owner, owner, this));
+
+            AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(null, DamageInfo.createDamageMatrix(amount2, true), DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+            for (AbstractMonster mo : AbstractDungeon.getMonsters().monsters) {
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(mo, owner, new WeakPower(mo, amount, false), amount, true, AbstractGameAction.AttackEffect.NONE));
+            }
+        }
+    }
+}
