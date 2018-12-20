232cf363c8692e694799edd70f9a3c7d201a1881
Kiooeht <kiooeht@gmail.com>
2018-12-19 21:28:12 -0800

    Card: Phantasmal Force

---
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/cards/PhantasmalForce.java
@@ -0,0 +1,76 @@
+package com.evacipated.cardcrawl.mod.bard.cards;
+
+import com.evacipated.cardcrawl.mod.bard.BardMod;
+import com.evacipated.cardcrawl.mod.bard.characters.Bard;
+import com.evacipated.cardcrawl.mod.bard.notes.AbstractNote;
+import com.evacipated.cardcrawl.mod.bard.notes.AttackNote;
+import com.evacipated.cardcrawl.mod.bard.notes.BlockNote;
+import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
+import com.evacipated.cardcrawl.mod.stslib.patches.core.AbstractCreature.TempHPField;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PhantasmalForce extends AbstractBardCard
+{
+    public static final String ID = BardMod.makeID("PhantasmalForce");
+    public static final String IMG = null;
+    private static final int COST = 2;
+    private static final int TEMP_HP = 7;
+    private static final int UPGRADE_TEMP_HP = 2;
+
+    public PhantasmalForce()
+    {
+        super(ID, IMG, COST, CardType.ATTACK, Bard.Enums.COLOR, CardRarity.COMMON, CardTarget.ENEMY);
+
+        magicNumber = baseMagicNumber = TEMP_HP;
+    }
+
+    @Override
+    public List<AbstractNote> getNotes()
+    {
+        return Arrays.asList(
+                new BlockNote(),
+                new AttackNote()
+        );
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        addToBottom(new AddTemporaryHPAction(p, p, magicNumber));
+        addToBottom(new AbstractGameAction()
+        {
+            @Override
+            public void update()
+            {
+                if (m.hasPower(WeakPower.POWER_ID)) {
+                    addToTop(new DamageAction(m, new DamageInfo(p, TempHPField.tempHp.get(p), damageTypeForTurn), AttackEffect.POISON));
+                }
+                isDone = true;
+            }
+        });
+    }
+
+    @Override
+    public void upgrade()
+    {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_TEMP_HP);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy()
+    {
+        return new PhantasmalForce();
+    }
+}
