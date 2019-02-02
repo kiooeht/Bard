de30927678007eb8b064d165b02ff498dc4040db
Kiooeht <kiooeht@gmail.com>
2019-02-01 23:08:32 -0800

    Card: Flourish

---
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/cards/Flourish.java
@@ -0,0 +1,60 @@
+package com.evacipated.cardcrawl.mod.bard.cards;
+
+import com.evacipated.cardcrawl.mod.bard.BardMod;
+import com.evacipated.cardcrawl.mod.bard.actions.common.SelectNoteAction;
+import com.evacipated.cardcrawl.mod.bard.characters.Bard;
+import com.evacipated.cardcrawl.mod.bard.notes.AbstractNote;
+import com.evacipated.cardcrawl.mod.bard.notes.AttackNote;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Flourish extends AbstractBardCard
+{
+    public static final String ID = BardMod.makeID("Flourish");
+    public static final String IMG = null;
+    private static final int COST = 1;
+    private static final int DAMAGE = 10;
+    private static final int UPGRADE_DAMAGE = 3;
+
+    public Flourish()
+    {
+        super(ID, IMG, COST, CardType.ATTACK, Bard.Enums.COLOR, CardRarity.UNCOMMON, CardTarget.ENEMY);
+
+        baseDamage = DAMAGE;
+    }
+
+    @Override
+    public List<AbstractNote> getNotes()
+    {
+        return Collections.singletonList(new AttackNote());
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        addToBottom(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+        addToBottom(new SelectNoteAction());
+    }
+
+    @Override
+    public void upgrade()
+    {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_DAMAGE);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy()
+    {
+        return new Flourish();
+    }
+}
