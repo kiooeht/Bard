e3a2a19ff56aeb665f7a87fe0d422f96c7af192b
Kiooeht <kiooeht@gmail.com>
2019-02-04 19:56:48 -0800

    Card: Fermata

---
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/cards/Fermata.java
@@ -0,0 +1,58 @@
+package com.evacipated.cardcrawl.mod.bard.cards;
+
+import com.evacipated.cardcrawl.mod.bard.BardMod;
+import com.evacipated.cardcrawl.mod.bard.actions.common.QueueNoteAction;
+import com.evacipated.cardcrawl.mod.bard.characters.Bard;
+import com.evacipated.cardcrawl.mod.bard.notes.AbstractNote;
+import com.evacipated.cardcrawl.mod.bard.notes.RestNote;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Fermata extends AbstractBardCard
+{
+    public static final String ID = BardMod.makeID("Fermata");
+    public static final String IMG = null;
+    private static final int COST = 1;
+    private static final int BLOCK = 11;
+    private static final int UPGRADE_BLOCK = 3;
+
+    public Fermata()
+    {
+        super(ID, IMG, COST, CardType.SKILL, Bard.Enums.COLOR, CardRarity.COMMON, CardTarget.SELF);
+
+        baseBlock = BLOCK;
+    }
+
+    @Override
+    public List<AbstractNote> getNotes()
+    {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        addToBottom(new GainBlockAction(p, p, block));
+        addToBottom(new QueueNoteAction(new RestNote()));
+    }
+
+    @Override
+    public void upgrade()
+    {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_BLOCK);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy()
+    {
+        return new Fermata();
+    }
+}
