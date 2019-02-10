4836bfa011f316f07daab49d87375b4900733085
Kiooeht <kiooeht@gmail.com>
2019-02-09 22:19:20 -0800

    Card: Solo

---
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/cards/Solo.java
@@ -0,0 +1,70 @@
+package com.evacipated.cardcrawl.mod.bard.cards;
+
+import com.evacipated.cardcrawl.mod.bard.BardMod;
+import com.evacipated.cardcrawl.mod.bard.actions.common.SelectMelodyAction;
+import com.evacipated.cardcrawl.mod.bard.characters.Bard;
+import com.evacipated.cardcrawl.mod.bard.helpers.MelodyManager;
+import com.evacipated.cardcrawl.mod.bard.notes.AbstractNote;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.ThoughtBubble;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Solo extends AbstractBardCard
+{
+    public static final String ID = BardMod.makeID("Solo");
+    public static final String IMG = null;
+    private static final int COST = 1;
+    private static final int UPGRADE_COST = 0;
+
+    public Solo()
+    {
+        super(ID, IMG, COST, CardType.SKILL, Bard.Enums.COLOR, CardRarity.UNCOMMON, CardTarget.SELF);
+    }
+
+    @Override
+    public List<AbstractNote> getNotes()
+    {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        addToBottom(new AbstractGameAction()
+        {
+            @Override
+            public void update()
+            {
+                if (p instanceof Bard) {
+                    if (((Bard) p).noteQueueSize() == 0) {
+                        addToTop(new SelectMelodyAction(MelodyManager.getAllMelodies(), false));
+                    } else {
+                        AbstractDungeon.effectList.add(new ThoughtBubble(p.dialogX, p.dialogY, 3.0f, EXTENDED_DESCRIPTION[0], true));
+                    }
+                }
+                isDone = true;
+            }
+        });
+    }
+
+    @Override
+    public void upgrade()
+    {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADE_COST);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy()
+    {
+        return new Solo();
+    }
+}
