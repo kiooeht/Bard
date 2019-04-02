218dc81cd2d0f0057bc00818c023ee24084476fa
Kiooeht <kiooeht@gmail.com>
2019-04-01 23:31:25 -0700

    Shrink title text for Mantle of Inspiration

--- src/main/java/com/evacipated/cardcrawl/mod/bard/cards/MantleOfInspiration.java
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/cards/MantleOfInspiration.java
@@ -28,6 +28,12 @@ public class MantleOfInspiration extends AbstractBardCard
     }
 
     @Override
+    public float getTitleFontSize()
+    {
+        return 20;
+    }
+
+    @Override
     public List<AbstractNote> getNotes()
     {
         return Collections.singletonList(new BuffNote());
