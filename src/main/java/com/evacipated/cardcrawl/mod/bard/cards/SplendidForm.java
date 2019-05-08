e055a89a13c804c754d6c7d896a33ddaa6ced00e
Kiooeht <kiooeht@gmail.com>
2019-05-08 00:14:43 -0700

    Easter egg

--- src/main/java/com/evacipated/cardcrawl/mod/bard/cards/SplendidForm.java
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/cards/SplendidForm.java
@@ -1,6 +1,7 @@
 package com.evacipated.cardcrawl.mod.bard.cards;
 
 import basemod.helpers.BaseModCardTags;
+import com.badlogic.gdx.graphics.Texture;
 import com.evacipated.cardcrawl.mod.bard.BardMod;
 import com.evacipated.cardcrawl.mod.bard.characters.Bard;
 import com.evacipated.cardcrawl.mod.bard.notes.AbstractNote;
@@ -9,6 +10,8 @@ import com.evacipated.cardcrawl.mod.bard.powers.SplendidPower;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import java.util.Collections;
@@ -20,14 +23,28 @@ public class SplendidForm extends AbstractBardCard
     private static final int COST = 3;
     private static final int INSPIRATION = 100;
     private static final int UPGRADE_INSPIRATION = 100;
+    private static final String ___ = new String(new byte[]{0x47, 0x72, 0x65, 0x6d, 0x69, 0x6f, 0x75, 0x73});
 
     public SplendidForm()
     {
         super(ID, COST, CardType.POWER, Bard.Enums.COLOR, CardRarity.RARE, CardTarget.SELF);
+        if (CardCrawlGame.playerName.equals(___)) {
+            RegionName regionName = getRegionName(cardID, type);
+            loadCardImage(regionName.name + "g");
+        }
 
         tags.add(BaseModCardTags.FORM);
 
         magicNumber2 = baseMagicNumber2 = INSPIRATION;
+    }
+
+    @Override
+    protected Texture getPortraitImage()
+    {
+        if (CardCrawlGame.playerName.equals(___)) {
+            return ImageMaster.loadImage(BardMod.assetPath(String.format("images/1024Portraits/%sg.png", getBaseImagePath(cardID, type))));
+        }
+        return super.getPortraitImage();
     }
 
     @Override
