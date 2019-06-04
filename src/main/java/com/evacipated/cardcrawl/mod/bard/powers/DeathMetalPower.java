666ae3581fd24f111523426cdc47eed2a8406d7e
Kiooeht <kiooeht@gmail.com>
2019-06-03 17:41:33 -0700

    Give Death Metal a vfx

--- src/main/java/com/evacipated/cardcrawl/mod/bard/powers/DeathMetalPower.java
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/powers/DeathMetalPower.java
@@ -3,14 +3,16 @@ package com.evacipated.cardcrawl.mod.bard.powers;
 import com.evacipated.cardcrawl.mod.bard.BardMod;
 import com.evacipated.cardcrawl.mod.bard.hooks.OnNoteQueuedHook;
 import com.evacipated.cardcrawl.mod.bard.notes.AbstractNote;
+import com.evacipated.cardcrawl.mod.bard.vfx.combat.ThrowNoteEffect;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
-import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
 
 public class DeathMetalPower extends AbstractBardPower implements OnNoteQueuedHook
 {
@@ -49,6 +51,18 @@ public class DeathMetalPower extends AbstractBardPower implements OnNoteQueuedHo
                         AbstractGameAction.AttackEffect.BLUNT_HEAVY
                 )
         );
+
+        float stagger = 0;
+        for (AbstractGameEffect effect : AbstractDungeon.effectList) {
+            if (effect instanceof ThrowNoteEffect) {
+                float f = ((ThrowNoteEffect) effect).getStaggerTimer();
+                if (f >= stagger) {
+                    stagger = f + (Settings.FAST_MODE ? 0.05f : 0.15f);
+                }
+            }
+        }
+        AbstractDungeon.effectList.add(new ThrowNoteEffect(note, owner.hb.cX, owner.hb.cY, Settings.WIDTH, owner.hb.cY, stagger));
+
 
         return null;
     }
