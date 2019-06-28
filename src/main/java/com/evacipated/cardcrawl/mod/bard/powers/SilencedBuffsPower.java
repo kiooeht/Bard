7448cc965bd9f106622a117819b071b877ebd313
Kiooeht <kiooeht@gmail.com>
2019-06-27 17:09:08 -0700

    Revert "Fix for new onRecevingPower hook"
    This reverts commit 4e79ae6a6a1dc1a93dc16ce96b8619811ff28fb8.

--- src/main/java/com/evacipated/cardcrawl/mod/bard/powers/SilencedBuffsPower.java
+++ src/main/java/com/evacipated/cardcrawl/mod/bard/powers/SilencedBuffsPower.java
@@ -1,6 +1,7 @@
 package com.evacipated.cardcrawl.mod.bard.powers;
 
 import com.evacipated.cardcrawl.mod.bard.BardMod;
+import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.OnReceivePowerPower;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
@@ -10,7 +11,7 @@ import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 
-public class SilencedBuffsPower extends AbstractBardPower
+public class SilencedBuffsPower extends AbstractBardPower implements OnReceivePowerPower
 {
     public static final String POWER_ID = BardMod.makeID("SilencedBuffs");
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
@@ -73,7 +74,7 @@ public class SilencedBuffsPower extends AbstractBardPower
     }
 
     @Override
-    public boolean onReceivingPower(AbstractPower power, AbstractCreature target, AbstractCreature source)
+    public boolean onReceivePower(AbstractPower power, AbstractCreature target, AbstractCreature source)
     {
         if (power.type == PowerType.BUFF && source == owner) {
             flash();
